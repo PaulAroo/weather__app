a1d6b73c89167300d851925316a2a84b26d27d08
Paul_Aroo <timmypaul2006@gmail.com>
2022-04-19 22:07:09 +0100

    update api route and validate input

--- s/App.js
+++ s/App.js
@@ -6,38 +6,26 @@ import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const [longLatList, setLongLatList] = useState([
-    { lat: 6.4550575, lon: 3.3941795 },
-    { lat: 9.0643305, lon: 7.4892974 },
-    { lat: 7.1, lon: 4.8333 },
-    { lat: 38.2542376, lon: -85.759407 },
-    { lat: 48.4283182, lon: -123.3649533 },
-    { lat: 40.6526006, lon: -73.9497211 },
-  ]);
   const [weatherList, setWeatherList] = useState([]);
-  const [inputVal, setInputVal] = useState("");
-
-  const getLatandLon = async (inputVal) => {
-    if (inputVal) {
-      const res = await fetch(
-        `https://api.openweathermap.org/geo/1.0/direct?q=${inputVal}&limit=1&appid=ad845893bc6a907b577994cbc54aa1a2`
-      );
-      const [resJson] = await res.json();
-      setLongLatList([{ lat: resJson.lat, lon: resJson.lon }]);
-      setInputVal("");
-    }
-  };
+  const [inputValue, setInputVal] = useState("");
+  const [validInput, setValidInput] = useState("");
+  const [locationList, setLocationList] = useState([
+    "brooklyn",
+    "alaska",
+    "louisville",
+    "abuja",
+    "lagos",
+    "ondo",
+  ]);
 
   useEffect(() => {
-    longLatList.forEach((location) =>
-      getWeatherData(location.lat, location.lon)
-    );
+    locationList.forEach((location) => getWeatherData(location));
     setLoading(false);
-  }, [longLatList]);
+  }, [locationList]);
 
-  const getWeatherData = async (lat, lon) => {
+  const getWeatherData = async (location) => {
     const data = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ad845893bc6a907b577994cbc54aa1a2&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ad845893bc6a907b577994cbc54aa1a2&units=metric`
     );
 
     const jsonData = await data.json();
@@ -55,6 +43,7 @@ const App = () => {
       },
       ...prev,
     ]);
+    setInputVal("");
   };
 
   const RenderResults = () => {
@@ -78,6 +67,15 @@ const App = () => {
       <p>loading...</p>
     );
   };
+
+  const validateInput = () => {
+    !inputValue
+      ? setValidInput("field cannot be empty")
+      : locationList.includes(inputValue)
+      ? setValidInput("data for input already exists")
+      : setLocationList([inputValue]);
+  };
+
   return (
     <div className="max-w-[1440px] mx-auto px-4 sm:px-12 pt-8">
       <h1 className="mb-12 bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-violet-500 text-5xl text-center ">
@@ -87,17 +85,25 @@ const App = () => {
         className="mb-8 flex flex-col md:flex-row gap-5"
         onSubmit={(e) => {
           e.preventDefault();
-          getLatandLon(inputVal);
+          validateInput();
         }}
       >
-        {/* <label>Enter the name of a country or a city</label> */}
-        <input
-          placeholder="name (e.g london)"
-          className="rounded-3xl w-[95%] md:w-1/2 lg:w-4/12 px-7 py-3 text-lg self-center"
-          type="text"
-          value={inputVal}
-          onChange={(e) => setInputVal(e.target.value)}
-        />
+        <div className="w-[95%] md:w-1/2 lg:w-4/12">
+          <input
+            placeholder="name (e.g london)"
+            className={`rounded-3xl w-full px-7 py-3 text-lg self-start ${
+              validInput && "border-red-600 border-2"
+            }`}
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputVal(e.target.value.toLowerCase())}
+          />
+          {validInput && (
+            <p className="text-center md:text-left py-1 px-4 text-red-600">
+              {validInput}
+            </p>
+          )}
+        </div>
         <motion.button
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
